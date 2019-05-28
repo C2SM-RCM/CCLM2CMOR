6b33c389d8afb6ab6c800720fea81ed458bef954
Marie-Estelle Demory <marie-estelle.demory@env.ethz.ch>
2019-05-28 15:50:58 +0200

    timeseries for COSMO-crCLIM CORDEX-CORE

---
+++ src/cclm_post/timeseries_cosmo-crclim.sh
@@ -0,0 +1,61 @@
+timeseries  W_SNOW     out01
+timeseries  W_SO       out01
+timeseries  T_S        out01
+timeseries  W_SO_ICE   out01
+timeseries  SNOW_CON   out03
+timeseries  RAIN_CON   out03
+timeseries  SNOW_GSP   out03
+timeseries  TOT_PREC   out03
+timeseries  CLCT       out03
+timeseries  CLCL       out03
+timeseries  CLCM       out03
+timeseries  CLCH       out03
+timeseries  T_2M       out03
+timeseries  TD_2M      out03
+timeseries  PMSL       out04
+timeseries  ASHFL_S    out04
+timeseries  U_10M      out04
+timeseries  V_10M      out04
+timeseries  ATHD_S     out04
+timeseries  ASWDIFD_S  out04
+timeseries  ASWDIR_S   out04
+timeseries  ATHU_S     out04
+timeseries  ASWDIFU_S  out04
+timeseries  QV_2M      out04
+timeseries  ASOB_T     out04
+timeseries  ASOD_T     out04
+timeseries  ALHFL_S    out04
+timeseries  PS         out04
+timeseries  DURSUN     out04
+timeseries  RELHUM_2M  out04
+timeseries  ATHB_T     out04
+timeseries  AEVAP_S    out04
+timeseries  TQI        out05
+timeseries  RUNOFF_S   out05
+timeseries  TQV        out05
+timeseries  TQC        out05
+timeseries  HPBL       out05
+timeseries  RUNOFF_G   out05
+timeseries  SNOW_MELT  out05
+timeseries  H_SNOW     out05
+timeseries  TMIN_2M    out07
+timeseries  TMAX_2M    out07
+timeseries  VMAX_10M   out07
+timeseries  VABSMX_10M out07
+#timeseries  CAPE_ML      out??
+#
+PLEVS=(925. 850. 700. 600. 500. 400. 300. 200. 100.) 
+timeseriesp U          out06 
+PLEVS=(925. 850. 700. 600. 500. 400. 300. 200. 100.) 
+timeseriesp FI         out06 
+PLEVS=(925. 850. 700. 600. 500. 400. 300. 200. 100.) 
+timeseriesp V          out06 
+PLEVS=(925. 850. 700. 600. 500. 400. 300. 200. 100.) 
+timeseriesp QV         out06 
+PLEVS=(925. 850. 700. 600. 500. 400. 300. 200. 100.) 
+timeseriesp T          out06 
+#
+ZLEVS=(100.) 
+timeseriesz U          out08
+ZLEVS=(100.) 
+timeseriesz V          out08
